1057bca75065d95469705f5b71abf554714b8040
enko <pytlos1337@gmail.com>
2023-05-16 13:35:01 +0200

    Add loader component and style PokemonItem

--- src/components/PokemonItem.vue
+++ src/components/PokemonItem.vue
@@ -1,5 +1,23 @@
-<script setup></script>
+<script setup>
+const props = defineProps({
+	name: String,
+	imageUrl: String,
+	abilities: Array,
+});
+</script>
 
-<template></template>
+<template>
+	<div class="flex flex-col items-center gap-2 shadow-lg p-8 bg-white rounded-lg">
+		<img :src="props.imageUrl" alt="Pokemon Image" />
+		<h2 class="font-bold">{{ props.name.toUpperCase() }}</h2>
+		<div>
+			<h3 class="text-center">Abilities:</h3>
+			<ul class="text-gray-500">
+				<li class="text-center" v-for="ability in props.abilities">{{ ability.ability.name }}</li>
+			</ul>
+		</div>
+		<button class="bg-blue-600 text-white px-4 py-1 rounded-2xl">Save in Pokedex</button>
+	</div>
+</template>
 
 <style scoped></style>
--- src/components/PokemonList.vue
+++ src/components/PokemonList.vue
@@ -2,18 +2,21 @@
 import { ref } from 'vue';
 import { useAxios } from '../composables/useAxios';
 import SearchInput from './SearchInput.vue';
+import PokemonItem from './PokemonItem.vue';
+import Loader from './Loader.vue';
 
 const searchQuery = ref('');
 const pokemonData = ref(null);
 const evolutionData = ref([]);
 const allPokemonData = ref([]);
+
 const { isLoading, hasError, fetchData } = useAxios();
 
 const handleSearchQueryChange = (value) => {
 	searchQuery.value = value;
 };
 
-const fetchPokemon = async () => {
+const fetchPokemonData = async () => {
 	if (searchQuery.value === '') return;
 
 	const response = await fetchData(`https://pokeapi.co/api/v2/pokemon/${searchQuery.value.toLowerCase()}`);
@@ -31,8 +34,6 @@ const fetchPokemon = async () => {
 	evolutionData.value = extractEvolutions(evolutionChainResponse.chain);
 
 	await fetchAllPokemonData(evolutionData.value);
-
-	searchQuery.value = '';
 };
 
 const extractEvolutions = (chain) => {
@@ -68,19 +69,29 @@ const fetchAllPokemonData = async (evolutionNames) => {
 	<div class="flex flex-col justify-center items-center h-screen">
 		<h1 class="font-bold text-3xl m-5">Gotta Catch 'em All!</h1>
 		<div class="flex gap-1">
-			<SearchInput @queryChange="handleSearchQueryChange" class="w-96" />
-			<button class="bg-blue-600 rounded-md px-8 py-2 text-white" @click="fetchPokemon">Search</button>
+			<SearchInput @queryChange="handleSearchQueryChange" class="w-96" @keyup.enter="fetchPokemonData" />
+			<button class="bg-blue-600 rounded-md px-12 py-2 text-white" @click="fetchPokemonData">Search</button>
 		</div>
-		<div class="min-h-[500px]">
-			<div v-if="isLoading">Loading...</div>
-			<div v-else-if="hasError">Error occurred while fetching data.</div>
+		<div class="min-h-[400px] flex justify-center items-center">
+			<div v-if="!allPokemonData.length && !isLoading && !hasError" class="self-start mt-10">
+				<p>Search for a Pokemon to see its details and evolution chain.</p>
+				<p>For example, try searching for 'pikachu'.</p>
+			</div>
 
-			<div v-else-if="allPokemonData">
-				<div class="flex">
-					<div v-for="pokemon in allPokemonData">
-						<h2>{{ pokemon.name }}</h2>
-						<img :src="pokemon.sprites.front_default" alt="Pokemon Image" />
-					</div>
+			<Loader v-if="isLoading" />
+			<div v-else-if="hasError" class="self-start mt-10">
+				<p>Error occurred while fetching data.</p>
+				<p>Try again!</p>
+			</div>
+
+			<div v-else-if="allPokemonData.length">
+				<div class="flex gap-10">
+					<PokemonItem
+						v-for="pokemon in allPokemonData"
+						:name="pokemon.name"
+						:imageUrl="pokemon.sprites.front_default"
+						:abilities="pokemon.abilities"
+					/>
 				</div>
 			</div>
 		</div>
